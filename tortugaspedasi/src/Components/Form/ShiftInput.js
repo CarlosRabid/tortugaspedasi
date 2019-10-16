6ccb963796995d2574672d8caae17ff618745b49
[Benji] <benjamin_landis92@yahoo.com>
2019-10-16 16:16:04 -0500

    cross your fingers

--- tortugaspedasi/src/Components/Form/ShiftInput.js
+++ tortugaspedasi/src/Components/Form/ShiftInput.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 
-class PersonInput extends Component {
+class ShiftInput extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            firstName: "",
+            lastName: "",
+            date: ""
+         }
     }
+
+    handleInput = (event) => {
+        this.setState({[event.target.name]: event.target.value}, ()=>console.log(this.state))
+    }
+    addShift = () => {
+        this.props.addShift(this.state.firstName, this.state.lastName, this.state.date)
+    }
+
     render() { 
-        return ( 
-            {/* Your Code Here */}
-         );
+        let input = ["First Name", "Last Name", "Date"]
+        return(
+        <div className="shift-container">
+                <h2>Shift</h2>
+                    <div className="shift-component">
+                        {input.map((i)=> <div className="new-shift-grid">
+                            <span className="new-shift-1"> {i}: </span>
+                            <input className="searchInput underline new-shift-2" 
+                                   name={i} onChange={this.handleInput}></input>
+                        </div>
+                        )}    
+                    </div>
+            </div> 
+        );
     }
 }
  
-export default PersonInput;
+export default ShiftInput;
