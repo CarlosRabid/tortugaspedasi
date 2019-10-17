b5f78c72ebbdd7679d9c68907e408e4dfd2aee32
fnando <fnando.gomez@gmail.com>
2019-10-17 12:21:05 -0500

    Geolocation moved to ObservationInput

--- tortugaspedasi/src/App.js
+++ tortugaspedasi/src/App.js
@@ -1,36 +1,19 @@
 import React, { Component } from 'react';
 import Landing from './Components/Landing';
-
 import NavBar from './Components/NavBar';
+import ObservationInput from './Components/Form/ObservationInput';
+
 import './App.css';
 
+
 class App extends Component {
-  
-  getMyPosition() {
-    function locationReceived(position){
-      console.log(position.coords.latitude)
-    }
-
-    function notReceived(errorPosition){
-        console.log("It's not possible to get your position")
-    }
-    
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(locationReceived, notReceived, );
-      console.log("True")
-    }else{
-      console.log("Geolocation not enable in this device")
-
-    }
-  }
 
   render() {
-
-    this.getMyPosition()
-
+    
     return (<div>
       <NavBar />
       {/* <Landing/>  */}
+      {/* <ObservationInput/> */}
     </div>)
   }
 }
--- tortugaspedasi/src/Components/Form/ObservationInput.js
+++ tortugaspedasi/src/Components/Form/ObservationInput.js
@@ -5,14 +5,41 @@ class ObservationInput extends Component {
         this.state = {  }
     }
 
+    getPosition() {
+        function geoSucess(position){
+          let geoCoords= {
+            lat:position.coords.latitude,
+            long:position.coords.longitude
+          }
+          alert(`This are your coordinates: - Latitude: ${geoCoords.lat} - Longitude: ${geoCoords.long}`)
+          return(geoCoords)
+        }
+    
+        function geoError(errorPosition){
+            alert("Error - Not position available")
+        }
+    
+        const geoOptions={
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 25000
+        }
+        
+        if(navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(geoSucess, geoError,geoOptions );
+          console.log("True")
+        }else{
+          console.log("Geolocation not enable in this device")
+    
+        }
+      }
   
 
     render() {
-
-        
-        
         return ( 
-            {/* Your Code Here */}
+            <div>
+                <button id="button" onClick={this.getPosition}>Click here to know your position</button>
+            </div>
          );
     }
 }
