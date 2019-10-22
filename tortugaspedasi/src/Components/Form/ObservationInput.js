811312eacb1e80690e306226fd8827ac5c371d3b
Simona Bali <simona.bali@outlook.com>
2019-10-21 19:10:46 -0500

    obs date and time

--- tortugaspedasi/src/Components/Form/ObservationInput.js
+++ tortugaspedasi/src/Components/Form/ObservationInput.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+
+
+let beaches = [
+    {
+        name: "Playa El Toro",
+        latitude: 7.530178,
+        longitude: -80.003034
+    },
+    {
+        name: "Playa El Arenal",
+        latitude: 7.559493,
+        longitude: -80.019275
+    },
+    {
+        name: "Playa Lagarto",
+        latitude: 7.506391,
+        longitude: -79.999284
+    }
+
+
+]
+
 class ObservationInput extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            date: "",
             time: "", // placeholder but editable
-            location: "",  // comes from geolocation, editable
-            comments: "" //free input
+            locationLatitude: "",  // set-up by beach selection
+            locationLongitude: "", // set-up by beach selection
+            comments: "", //free input
+
         }
+    }
+    handleLocation = async (event) => {
+        let currentBeach = event.target.value ? beaches.find(b => b.name === event.target.value) : { latitude: "", longitude: "" }
+        let locationLatitude = { ...this.state.locationLatitude }
+        let locationLongitude = { ...this.state.locationLongitude }
+        locationLatitude = currentBeach.latitude
+        locationLongitude = currentBeach.longitude
+        await this.setState({ locationLatitude, locationLongitude })
     }
 
     handleInput = (event) => {
@@ -16,64 +49,60 @@ class ObservationInput extends Component {
         this.props.addShift(this.state.time, this.state.location, this.state.comments)
     }
 
-    getPosition() {
-        function geoSucess(position) {
-            let geoCoords = {
-                lat: position.coords.latitude,
-                long: position.coords.longitude
-            }
-            alert(`This are your coordinates: - Latitude: ${geoCoords.lat} - Longitude: ${geoCoords.long}`)
-            return (geoCoords)
-        }
-
-        function geoError(errorPosition) {
-            alert("Error - Not position available")
-        }
-
-        const geoOptions = {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 25000
-        }
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(geoSucess, geoError, geoOptions);
-            console.log("True")
-        } else {
-            console.log("Geolocation not enable in this device")
-
-        }
+    componentDidMount = () => {
+        this.setState({date: new Date()})
     }
 
+    // getDate2 = () => {
+    //     let currentDate = new Date();
+    //     let date = currentDate.getDate();
+    //     let month = currentDate.getMonth();
+    //     let year = currentDate.getFullYear();
+    //     let monthDateYear = (month + 1) + "/" + date + "/" + year;
+    //     return monthDateYear
+    // }
 
+    // getDate = () => {
+    //     let today = new Date().toLocaleDateString(undefined, {
+    //         day: '2-digit',
+    //         month: '2-digit',
+    //         year: 'numeric'
+    //     })
+    //     return today
+    // }
 
     render() {
-        let input = [{formName:"Time", stateName: "time"}, {formName:"Location", stateName: "location"},{formName:"Comments", stateName: "moonPhase"} ]
+        let date = new Date()
         return (
             <div className="observation-container">
                 <h2>Observation</h2>
-                <div className="observation-component">
-                    <span className="new-observation-1">Location:</span>
-                    <input className="searchInput underline new-observation-2"
-                            name="location" onChange={this.handleInput}></input>
-                    <button id="button" onClick={this.getPosition}>Click here to know your position</button>
-                    
-                </div>
-                <div className="observation-component">
-                    {input.map((i) => <div className="new-observation-grid">
-                        <span className="new-observation-1"> {i.formName}: </span>
-                        <input className="searchInput underline new-observation-2"
-                            name={i.stateName} onChange={this.handleInput}></input>
 
+                <form >
+                    <div>Date: <input type="date" name="date" onChange={this.handleInput} value={this.getdate} ></input></div>
+                    <div>Time: <input type="time" name="time" onChange={this.handleInput}></input></div>
+                </form>
+
+
+                <div className="observation-component">
+                    <div>
+                        <span>Location:</span>
+                        <select onChangeCapture={this.handleLocation}>
+                            <option value="" >Select</option>
+                            <option value="Playa El Arenal"  >Playa El Arenal</option>
+                            <option value="Playa El Toro" >Playa El Toro</option>
+                            <option value="Playa Lagarto" >Playa Lagarto</option>
+                        </select>
                     </div>
-                    )}
-                    
+                </div>
 
+                <div className="observation-component">Comments:
+                <input className="searchInput-observationInput"
+                        name="comments" onChange={this.handleInput}></input>
                 </div>
             </div>
         );
 
     }
 }
- 
+
 export default ObservationInput;
