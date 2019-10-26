a0e711f21949cfc4794dda629c188d6dee41dfa8
[Benji] <benjamin_landis92@yahoo.com>
2019-10-26 17:09:35 -0500

    isLoggedIn to localStorage

--- tortugaspedasi/src/Components/Login.js
+++ tortugaspedasi/src/Components/Login.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Landing from './Landing'; // do NOT delete!
 
 let credentials = {
-    Admin1: "TSDqmaPu",
-    Admin2: "ngEyYWTp",
-    Admin3: "3fv9fZGL",
-    Admin4: "zwvY5Tjt",
-    Admin5: "wZXaHrxF",
+    Robert: "TSDqmaPu",
+    Isabelle: "ngEyYWTp",
+    Guest1: "3fv9fZGL",
+    Guest2: "zwvY5Tjt",
+    Guest3: "wZXaHrxF",
 }
 
 class Login extends Component {
@@ -15,7 +15,8 @@ class Login extends Component {
         super()
         this.state = {
             admin: {
-                user: '',
+                firstName: '',
+                lastName: '',
                 password: '',
                 rememberMe: false,
                 isLoggedIn: false
@@ -28,29 +29,40 @@ class Login extends Component {
         const input = event.target;
         const value = input.type === 'checkbox' ? input.checked : input.value;
 
+
         this.setState({ [input.name]: value });
     };
 
     handleFormSubmit = () => {
-        const { user, password, rememberMe } = this.state;
+        const {isLoggedIn, firstName, lastName, password, rememberMe } = this.state;
         localStorage.setItem('rememberMe', rememberMe);
-        localStorage.setItem('user', rememberMe ? user : '');
+        localStorage.setItem('isLoggedIn', rememberMe ? isLoggedIn: true);
+        localStorage.setItem('firstName', rememberMe ? firstName: '');
+        localStorage.setItem('lastName', rememberMe ? lastName : '');
         localStorage.setItem('password', rememberMe ? password : '');
     };
 
     componentDidMount() {
         const rememberMe = localStorage.getItem('rememberMe') === 'true';
-        const user = rememberMe ? localStorage.getItem('user') : '';
+        const firstName = rememberMe ? localStorage.getItem('firstName') : '';
+        const lastName = rememberMe ? localStorage.getItem('lastName') : '';
         const password = rememberMe ? localStorage.getItem('password') : '';
-        this.setState({ user, password, rememberMe });
+        this.setState({ firstName, lastName, password, rememberMe });
     };
 
     login = (event) => {
 
-        if (credentials[this.state.user] === this.state.password) {
-            let admin = { user: this.state.user, password: this.state.password, rememberMe: true, isLoggedIn: true }
+        if (credentials[this.state.firstName] === this.state.password) {
+            let admin = {
+                    firstName: this.state.firstName, 
+                    lastName: this.state.lastName, 
+                    password: this.state.password, 
+                    rememberMe: true, 
+                    isLoggedIn: true }
             this.setState({ admin: admin })
             localStorage.admin = JSON.stringify(admin)
+
+            // not using but cannot delete the code below //
             event.preventDefault();
             let username = event.target[0].value
             this.props.updateUser(username)
@@ -82,15 +94,18 @@ class Login extends Component {
             <div>
                 <form onSubmit={this.loginFuncs} >
                     <label>
-                        User: <input name="user" value={this.state.user} onChange={this.handleChange} />
+                        First Name: <input name="firstName" value={this.state.firstName} onChange={this.handleChange} />
                     </label>
                     <label>
-                        Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                        Surname: <input name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Password: <input  name="password" type="password" value={this.state.password} onChange={this.handleChange} />
                     </label>
                     <label>
                         <input name="rememberMe" checked={this.state.rememberMe} onChange={this.handleChange} type="checkbox" /> Remember me
                     </label>
-                    <button type="submit" >Sign In</button>
+                    <button type="submit">Sign In</button>
                 </form>
             </div>
 
