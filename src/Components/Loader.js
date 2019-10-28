2470e4ec651363af5c21350bb64c535009f8dd10
jona <jona@elevation.ac>
2019-10-28 16:18:48 +0200

    add loader to app and charts

---
+++ src/Components/Loader.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../styles/loader.css'
+
+class Loader extends Component {
+
+    render() {
+        return (
+            <div className="turtle-wrapper">
+                <section class="turtle">
+                    <div class="head"></div>
+                    <div class="legs-01"></div>
+                    <div class="legs-02"></div>
+                    <div class="tail"></div>
+                    <div class="shell">
+                        <div></div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Loader;
